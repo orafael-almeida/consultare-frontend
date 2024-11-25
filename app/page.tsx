abf99fd4a4f3a25d5bf900abc342bd13cdac8f37
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-25 11:40:19 -0300

    feat: add homepage structure and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-import {Button} from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: add OTP verification and Passkeymodal */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-end text-dark-600 xl:text-left">
+              © 2024 Consultare
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.webp"
+        height={1000}
+        width={1000}
+        alt="Medical Illustration"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
