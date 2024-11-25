0391c61d39aed9b1f3c3bf2fc921137db9f63ccf
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-25 14:10:25 -0300

    fixx: adjusts max length for the name input

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,12 +4,12 @@ export const UserFormValidation = z.object({
   name: z
     .string()
     .min(2, "O nome deve conter pelo menos 2 caracteres.")
-    .max(2, "O nome deve conter no máximo 50 caracteres."),
+    .max(50, "O nome deve conter no máximo 50 caracteres."),
   email: z.string().email("E-mail inválido."),
   phone: z
     .string()
     .refine(
-      (phone) => /^\+\d{10,15}&/.test(phone),
+      (phone) => /^\+\d{10,15}$/.test(phone),
       "Número de telefone inválido."
     ),
 });
