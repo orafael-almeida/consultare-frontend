60b43f17ebdc6bcc44bce2616912daa9b2a67360
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-26 11:29:13 -0300

    fix: adjusts typing errors and classnames

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="logo"
+            alt="PAtient"
             className="mb-12 h-10 w-fit"
           />
 
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-end text-dark-600 xl:text-left">
+            <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 Consultare
             </p>
             <Link href="/?admin=true" className="text-green-500">
