2ec63bcba27de04f6c554e957c94ffefba6ce961
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-27 15:10:22 -0300

    feat: creates update-appointment server action

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -8,6 +8,7 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -72,6 +73,33 @@ export const getRecentAppointmentList = async () => {
     };
 
     return parseStringify(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updateAppointment) {
+      throw new Error("Appointment not found");
+    }
+
+    // TODO SMS notification
+
+    revalidatePath(`/admin`);
+    return parseStringify(updatedAppointment);
   } catch (error) {
     console.error(error);
   }
